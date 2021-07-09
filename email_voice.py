5ce0fe3ca3bb38d0d40ceb90c8235ceb45f57d0c
MauricioAliendre182 <maps.3167@gmail.com>
2021-07-09 19:15:35 -0400

    adding a new Python program

---
+++ email_voice.py
@@ -0,0 +1,48 @@
+import speech_recognition as sr
+import smtplib  # Protocol SMTP
+from email.message import EmailMessage
+
+email = EmailMessage()  # We will create the object 'email'
+r = sr.Recognizer()     # We will create the object 'r' to recognize your voice
+
+while True:
+    with sr.Microphone() as source:
+        print("Say your subject...")
+        audio = r.listen(source)
+
+    # Now we will put all the data to send the email
+    email['from'] = 'Name'
+    email['to'] = 'example@gmail.com'
+    try:
+        subject_1 = email['subject'] = r.recognize_google(
+            audio, language="en-US")
+        print(f'What did you say: {subject_1}')
+    except:
+        print('I am sorry! I can not understand!')
+        break
+
+    with sr.Microphone() as source_2:
+        print("Say the content of your email...")
+        audio_2 = r.listen(source_2)
+
+    try:
+        email_content = r.recognize_google(audio_2, language="en-US")
+        email.set_content(email_content)
+        print(
+            f'What did you say: {email_content}')
+    except:
+        print('I am sorry! I can not understand!')
+        break
+
+    # We will configure an SMTP client session object
+    with smtplib.SMTP(host='smtp.gmail.com', port=25) as smtp:
+        smtp.ehlo()      # This is kind of like 'Hello' message
+        smtp.starttls()  # TLS is an encryption mechanism
+        
+        # We have to login to our account to send the email
+        smtp.login('something@gmail.com', 'XXXXYYYYZZZ')
+        smtp.send_message(email)   # Send the message
+        print('all good boss!')
+        break
+
+# IMPORTANT_NOTE: The name of the file must be different to 'email'
